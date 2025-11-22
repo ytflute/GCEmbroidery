83bf872dcfaabef089f4473a197c8496304ce2e6
FutureThingsLab <futurethingslab0314@gmail.com>
2025-11-22 22:16:53 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { LanguageProvider } from "./contexts/LanguageContext";
-import { LanguageSwitcher } from "./components/LanguageSwitcher";
 import { Hero } from "./components/Hero";
 import { About } from "./components/About";
 import { Services } from "./components/Services";
@@ -12,7 +11,6 @@ export default function App() {
   return (
     <LanguageProvider>
       <div className="min-h-screen bg-white">
-        <LanguageSwitcher />
         <Hero />
         <About />
         <Services />
