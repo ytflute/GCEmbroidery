6a69d8d63be88764fb18b404b0ad5e4afd00a343
FutureThingsLab <futurethingslab0314@gmail.com>
2025-11-23 23:07:53 +0800

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -29,7 +29,7 @@ export function Hero() {
           <ImageWithFallback
             src="/images/title.png"
             alt="廣承綉花實業社"
-            className="mx-auto max-w-xs h-auto"
+            className="mx-auto max-w-xs max-h-48 w-auto h-auto object-contain"
           />
         </div>
         <Button
